153bb6ee1af1ba1c437f9eebf9ed042429421937
Vish0111 <it21315282@my.sliit.lk>
2024-04-17 03:37:54 +0530

    cache added

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,24 @@ function App() {
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+      //caching
+      const today = (new Date()).toString()
+      const localKey = `NASA-$(today)`
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log('Fetched from cache today')
+        return
+      }
+      localStorage.clear
+
       try {
         const res = await fetch(url)
         const apiData = await res.json()
+        localStorage.setItem(localKey,JSON.stringify(apiData))
         setData(apiData)
+        console.log('Fetched from API today')
         console.log('DATA\n', apiData)
       } catch (err) {
         console.log(err.message)

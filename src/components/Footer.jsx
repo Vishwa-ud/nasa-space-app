22a05f7207187f434c3005fe4a9e1fbe25fa5ac9
Vish0111 <it21315282@my.sliit.lk>
2024-04-17 04:36:10 +0530

    css in deferent file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 export default function Footer(props) {
 
     const { showModal, handleToggleModal, data } = props
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 export default function Sidebar(props) {
     const { handleToggleModal, data} = props
     return (

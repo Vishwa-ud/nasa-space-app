22f8c18b08b902d997c2f08987334cba8eae1e21
Vish0111 <it21315282@my.sliit.lk>
2024-04-26 16:40:42 +0530

    hosting

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connection();
 app.use(express.json());
 //app.use(cors());
 app.use(cors({
-    origin: 'https://nasa-space-app-server.vercel.app/', // Allow requests from localhost:8080
+    origin: 'https://nasa-space-app-frontend.vercel.app', // Allow requests from localhost:8080
     methods: 'GET,POST', // Allow all request methods
     credentials: true // Allow credentials
   }));

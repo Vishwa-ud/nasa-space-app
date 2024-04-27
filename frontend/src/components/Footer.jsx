e95b2b2bc6eca141f3f8a3acf0864c2a8c49da61
Vish0111 <it21315282@my.sliit.lk>
2024-04-27 19:13:47 +0530

    Download image from APOD

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
 import './styles.css';
 
-export default function Footer(props) {
-
-    const { showModal, handleToggleModal, data } = props
+export default function Footer({ showModal, handleToggleModal, data }) {
+    // Function to handle image download
+    const handleDownload = () => {
+        if (data && data.hdurl) {
+            // Create an anchor element
+            const downloadLink = document.createElement('a');
+            // Set the href attribute to the image URL
+            downloadLink.href = data.hdurl;
+            // Set the download attribute to force download
+            downloadLink.setAttribute('download', 'APOD_image.jpg');
+            // Simulate a click on the anchor element to trigger download
+            downloadLink.click();
+        }
+    };
 
     return (
         <footer>
@@ -11,10 +23,14 @@ export default function Footer(props) {
                 <h1>Astronomy Picture of the Day</h1>
                 <h2>{data?.title}</h2>
             </div>
-            <button onClick={handleToggleModal}>
+            <div className="buttonGroup">
+        <button className='donloadbtn' onClick={handleDownload}>
+            <i className="fa-regular fa-circle-down"></i>
+        </button>
+        <button onClick={handleToggleModal}>
             <i className="fa-solid fa-circle-info"></i>
-            </button>
-            
+        </button>
+    </div>
         </footer>
-    )
+    );
 }

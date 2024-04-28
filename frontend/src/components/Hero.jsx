0bcf0582f9cc694516bc923eb4cef871981745ce
Vish0111 <it21315282@my.sliit.lk>
2024-04-28 18:56:14 +0530

    Particles component added toHero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,6 +3,8 @@ import { styles } from "./styles";
 import backgroundImage from '../assets/background.jpg';
 import { grid, reuleaux } from 'ldrs';
 
+import Particle from './Particle';
+
 grid.register();
 reuleaux.register();
 
@@ -37,6 +39,7 @@ const Hero = () => {
     <section className={`relative w-full h-screen mx-auto`}
       style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
+      <Particle />
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className={'flex flex-col justify-center items-center mt-5'}>
           <div className={'w-5 h-5 rounded-full bg-[#915EFF]'} />
